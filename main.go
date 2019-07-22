d43850fe48136f1ad24a3863b7986788319ecc95
Devin J. Dawson <devinjdawson@me.com>
2019-07-21 20:19:26 -0400

    Update main.go

--- main.go
+++ main.go
@@ -447,16 +447,15 @@ func monitor(c config) {
 		}
 		db.Update(func(tx *bolt.Tx) error {
 			f := tx.Bucket([]byte("followers"))
-	
+
 			for _, v := range FtoAdd {
 				err := f.Put([]byte(v.uid), []byte(v.followedAt))
 				if err != nil {
 					return err
 				}
-
+			}
 			return nil
-		}
-      }
+			}
 	  db.Update(func(tx *bolt.Tx) error {
 		  o := tx.Bucket([]byte("follows"))
 
@@ -466,9 +465,8 @@ func monitor(c config) {
 				  return err
 			  }
 		  }
-
 		  return nil
-	  })
+	  		})
 		db.Close()
 	}
 
